38d514a4960c31ec0d78f14d7cc8f98a65df5b8a
Ady levy <adylevy@gmail.com>
2015-02-06 14:16:45 +0200

    case insensitive

--- modules/player_cls.js
+++ modules/player_cls.js
@@ -25,8 +25,8 @@ var Player = Class.extend(function () {
             }
             //line=line.toLowerCase();
             this.line = line;
-            var playerRgx = /(\d+)\s+([\w\-@\.,\s\(\)]{1,35})\s+([\d\.,\s?bmk]+)[\s?\/?,?(]+([\d\.,mk]+)[\s?\/?,?()]+([\d\.,mk]+)/;
-            var nameLvlRgx = /([\w\-@\.,\s\(\)]{1,35})\s+(\d+)\s+([\d\.,\s?bmk]+)[\s?\/?,?(]+([\d\.,mk]+)[\s?\/?,?()]+([\d\.,mk]+)/;
+            var playerRgx = /(\d+)\s+([\w\-@\.,\s\(\)]{1,35})\s+([\d\.,\s?mkbMKB]+)[\s?\/?,?(]+([\d\.,mkMK]+)[\s?\/?,?()]+([\d\.,mkMK]+)/;
+            var nameLvlRgx = /([\w\-@\.,\s\(\)]{1,35})\s+(\d+)\s+([\d\.,\s?mkbMKB]+)[\s?\/?,?(]+([\d\.,mkMK]+)[\s?\/?,?()]+([\d\.,mkMK]+)/;
             var matches = playerRgx.exec(line);
             var secondTry = nameLvlRgx.exec(line);
             if (matches==null && secondTry!=null){
