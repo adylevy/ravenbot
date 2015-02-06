3b65ace7091404cbeb3c2a470d2d00108a1d5e0e
Ady levy <adylevy@gmail.com>
2015-02-06 20:36:03 +0200

    fresh/old

--- modules/bot.js
+++ modules/bot.js
@@ -498,7 +498,7 @@ var Bot = BotBase.extend(function () {
                             _.each(candidates, function (candidate) {
                                 var crank = candidate.rank;
                                 var rank = crank > 2 ? 'A' : crank > 1.5 ? 'B' : 'C';
-                                msg.push(candidate.toString() + ' [' + candidate.origin + '|' + (candidate.isFresh() ? 'Old' : 'Fresh') + '|' + rank + ']');
+                                msg.push(candidate.toString() + ' [' + candidate.origin + '|' + (candidate.isFresh() ? 'Fresh': 'Old') + '|' + rank + ']');
                             });
 
                             this.postMessage(msg.join('\n'));
