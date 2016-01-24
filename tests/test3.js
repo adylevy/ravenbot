f78354df0f2700bc9505692a47d78fb9bf12cb1e
Ady Levy <adylevy@gmail.com>
2016-01-25 00:28:58 +0200

    timer

--- tests/test3.js
+++ tests/test3.js
@@ -1,16 +1,34 @@
 var roomPrefs = require('./../modules/data/roomPrefs.js');
-
-var whenConnected=function(){
+var _ = require('underscore');
+var whenConnected = function () {
     console.log('mongo is connected');
-    roomPrefs.getAllRoomPrefs().then(function(data){
+   // this.timerInterval = setInterval(function () {
 
-      console.log(data);
-   })
+   // }.bind(this),  1000);
+
+
+
+    var onTimeTick = function (){
+        roomPrefs.getAllRoomPrefs().then(function (rooms) {
+
+            _.each(rooms, function (room) {
+                if (room.warData.inWar) {
+                    //  console.log('onTick',room.roomId,this.allBots);
+                    console.log(room);
+                }
+            }.bind(this));
+
+         //   console.log(rooms);
+        });
+    }
+    onTimeTick()
+
+    ;
 
 };
 
 var mongoData = require('./../modules/data/mongoData.js');
-mongoData.on('mongoConnected',whenConnected);
+mongoData.on('mongoConnected', whenConnected);
 mongoData.connect();
 
 
